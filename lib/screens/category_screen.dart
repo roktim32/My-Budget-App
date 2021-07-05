e3a933f045a2fe9075320acf71cf001a220848a7
roktim32 <roktimsenapoty2@gmail.com>
2021-07-06 02:41:00 +0530

    category

---
+++ lib/screens/category_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_budget_ui/models/category_model.dart';
+
+class CategoryScreen extends StatefulWidget {
+  final Category category;
+
+  @override
+  _CategoryScreenState createState() => _CategoryScreenState();
+}
+
+class _CategoryScreenState extends State<CategoryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
