b0b19818c05d4685636fc1791ea279299bba9aa3
roktim32 <roktimsenapoty2@gmail.com>
2021-07-06 20:40:23 +0530

    category screen added items

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -14,6 +14,49 @@ class CategoryScreen extends StatefulWidget {
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
+  _buildExpenses() {
+    List<Widget> expenseList = [];
+    widget.category!.expenses!.forEach((Expense expense) {
+      expenseList.add(
+        Container(
+          alignment: Alignment.center,
+          margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+          height: 88.0,
+          width: double.infinity,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10.0),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black12,
+                    offset: Offset(0, 2),
+                    blurRadius: 6.0)
+              ]),
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  expense.name!,
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  '-\₹${expense.cost!.toStringAsFixed(2)}',
+                  style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w600),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    });
+    return Column(children: expenseList);
+  }
+
   @override
   Widget build(BuildContext context) {
     double totalAmountSpent = 0;
@@ -71,6 +114,7 @@ class _CategoryScreenState extends State<CategoryScreen> {
                 ),
               ),
             ),
+            _buildExpenses(),
           ],
         ),
       ),
